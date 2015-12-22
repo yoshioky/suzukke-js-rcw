40c06cdea37696183b4223d104c2aa488bc9d905
Kei Suzuki <kei.suzuki@justsystems.com>
2015-12-22 12:45:14 +0900

    spec3: レシピデータファイルの定義、読み込み処理の途中まで実装。

--- Recipe/main.cpp
+++ Recipe/main.cpp
@@ -6,6 +6,15 @@ int _tmain(int argc, _TCHAR* argv[])
 	// 日本語出力用のロケール設定
 	setlocale(LC_ALL, "ja-JP");
 
+	// 引数チェック
+	if (argc < 1) {
+		exit(-1);
+	}
+
+	// ファイル名
+	LPCWSTR filePath = argv[0];
+	// ToDo: ファイル読み込み
+
 	// レシピタイトルの出力
 	Recipe recipe(L"オムライス");
 	std::wcout << recipe.GetTitle();
